57ca03d368f6beb8b8548589b33ae748664fe54f
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-08 11:17:09 +0200

    Fix spelling: 'occurred' and whitespace
    Change-Id: I592ab9b521602efd388748a7c1f6e7fab9d94455

--- maintenance/maintainFarm.php
+++ maintenance/maintainFarm.php
@@ -2,11 +2,11 @@
 /**
  * Connect with one 'Simple Farm' farm member and start several maintenance
  * scripts from there to maintain several farm members at the same time.
- * 
+ *
  * @file
  * @ingroup Maintenance
  * @ingroup SimpleFarm
- * 
+ *
  * @author Daniel Werner < danweetz@web.de >
  */
 
@@ -16,15 +16,15 @@ require_once dirname( __FILE__ ) . '/../includes/SimpleFarmMember.php';
 
 /**
  * 'Simple Farm' maintenance handler
- * 
+ *
  * @since 0.1
  */
 class SimpleFarmerMaintenance extends Maintenance {
-	
+
 	function __construct() {
 		SimpleFarm::$maintenanceIsRunning = true;
 		parent::__construct();
-		
+
 		$this->mDescription = "'Simple Farm' farmer script for maintaining several farm members with just one command";
 		$this->addOption( 'farmexclude', 'comma-separated list of wikis to deselect for farming (by database name)' );
 		$this->addOption( 'farmonly', 'comma-separated list of wikis to select for doing some farming on (by database name)' );
@@ -33,23 +33,23 @@ class SimpleFarmerMaintenance extends Maintenance {
 		// TODO: add option to execute 'command' from mw root dir with PHP_BINARY (php 5.4). There is no nice way for
 		//       doing this in php 5.3 or earlier though.
 	}
-	
+
 	public function getDbType() {
 		return Maintenance::DB_NONE;
 	}
-	
+
 	public function execute() {
 		global $egSimpleFarmWikiSelectEnvVarName;
-		
+
 		$prfx = '~~ '; // prefix in front of each line to make an optical difference to pass through scripts
-		
+
 		$selected = SimpleFarm::getMembers();
 		$totalMembers = count( $selected );
-		
+
 		// farmexclude:
 		$excluded = preg_split( '/\s*,\s*/', trim( $this->getOption( 'farmexclude', '' ) ) );
 		$selected = array_diff_key( $selected, array_flip( $excluded ) );
-		
+
 		// farmonly:
 		if( $this->getOption( 'farmonly' ) !== null ) {
 			$only = preg_split( '/\s*,\s*/', trim( $this->getOption( 'farmonly', '' ) ) );
@@ -70,32 +70,32 @@ class SimpleFarmerMaintenance extends Maintenance {
 
 		// print selected members:
 		$longest = 0;
-		foreach( $selected as $member ) {				
+		foreach( $selected as $member ) {
 			if( ( $currLen = strlen( $member->getName() ) ) > $longest )
 				$longest = $currLen;
 		}
-		
+
 		$i = 0;
 		foreach( $selected as $member ) {
 			$i++;
 			$this->output( "\n{$prfx}($i) Name: " . str_pad( "'{$member->getName()}'", $longest + 2, ' ', STR_PAD_RIGHT ) . " DB: '{$member->getDB()}'" );
 		}
-		
+
 		// if we are in preview mode, just display what has been selected and end:
 		if( $this->getOption( 'farmpreview' ) !== null )
 			return; // no further action
-		
+
 		// finally, run the script for each:
 		$script = $this->getArg( 0 );
 		if( $script === null ) {
 			$this->error( "\n{$prfx}No maintaining command has been set! Argument <command> required!", true );
 		}
-		
+
 		$this->output( "\n\n{$prfx}Start maintaining selected 'Simple Farm' members:" );
 		$this->output( "\n{$prfx}Command: '{$script}'" );
 		$i = 0;
 		$failed = 0;
-		
+
 		foreach( $selected as $member ) {
 			$i++;
 			$this->output( "\n\n{$prfx}({$i}/{$selectedMembers}) Running command for member '{$member->getName()}' ({$egSimpleFarmWikiSelectEnvVarName}={$member->getDB()})...\n\n" );
@@ -108,7 +108,7 @@ class SimpleFarmerMaintenance extends Maintenance {
 		}
 
 		$this->output( "\n\n{$prfx}Finished! All {$selectedMembers} selected 'Simple Farm' members have been maintained" );
-		
+
 		// check whether we had any errors along the way:
 		if( $failed > 0 ) {
 			// send out some warning!
@@ -116,7 +116,7 @@ class SimpleFarmerMaintenance extends Maintenance {
 		}
 		else {
 			// success!
-			$this->output( "\n{$prfx}Apparently, no error has occured during command executions :-)\n" );
+			$this->output( "\n{$prfx}Apparently, no error has occurred during command executions :-)\n" );
 		}
 	}
 }
